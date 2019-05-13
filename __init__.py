b944037c3721b3a9d5b026581488cc96f83df01e
Deanna Green <deanna@16vmini.co.uk>
2019-05-13 12:28:57 +0100

    made the sign up method work so it adds user to database or shows flash message

--- __init__.py
+++ __init__.py
@@ -17,6 +17,12 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
     # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
