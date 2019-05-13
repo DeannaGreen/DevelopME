6d26053c5f53c924126bc1ae5ed5b4202a0c8fdf
Deanna Green <deanna@16vmini.co.uk>
2019-05-13 14:34:02 +0100

    users can log out and shows different buttons if they are logged in or out

--- auth.py
+++ auth.py
@@ -2,7 +2,7 @@ from . import db
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 auth = Blueprint('auth', __name__)
 
@@ -57,5 +57,7 @@ def signup_post():
 
 
 @auth.route('/logout')
+@login_required
 def logout():
+    logout_user()
     return render_template('logout.html')
