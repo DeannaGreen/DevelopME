7bf20b7f6501ea1054dc2f7d651579964a54e8af
Deanna Green <deanna@16vmini.co.uk>
2019-05-14 14:20:57 +0100

    prints random question to page

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from . import db
 from . import config
+import random
 
 main = Blueprint('main', __name__)
 
@@ -15,5 +16,18 @@ def profile():
     return render_template('profile.html', name=current_user.name)
 
 @main.route('/gif')
+@login_required
 def giphy():
     return render_template('gif.html', GIPHY_KEY=config.CODE_CONFIG['GIPHY_KEY'])
+
+@main.route('/questions')
+@login_required
+def questions():
+    questions = [
+    'Question1', 
+    'Question2',
+    'Question3'
+    ]
+    q = random.choice(questions)
+
+    return render_template('questions.html', q=q)
