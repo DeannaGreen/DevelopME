5233049d55bd93b761cdfc6ee6bd4099579a3d92
Deanna Green <deanna@16vmini.co.uk>
2019-05-21 10:05:05 +0100

    changed date format on newsfeed page

--- developme/newsfeed.py
+++ developme/newsfeed.py
@@ -4,6 +4,7 @@ from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from . import db
 import os
+from datetime import datetime
 
 from tweepy import Stream
 from tweepy import OAuthHandler
@@ -26,29 +27,20 @@ def tweets():
 
     twitter_api = twitter.Twitter(auth=auth)
 
-    # print(twitter_api)
-
     q = '#coding'
 
     count = 20
-
-    # See https://dev.twitter.com/docs/api/1.1/get/search/tweets
 
     search_results = twitter_api.search.tweets(q=q, count=count)
 
     statuses = search_results['statuses']
 
-    # print(statuses)
-
-    # Iterate through 5 more batches of results by following the cursor
-
     for _ in range(20):
         try:
             next_results = search_results['search_metadata']['next_results']
-        except KeyError: # No more results when next_results doesn't exist
+        except KeyError:
             break
 
-        # Create a dictionary from next_results, which has the following form:
         kwargs = dict([ kv.split('=') for kv in next_results[1:].split("&") ])
 
         search_results = twitter_api.search.tweets(**kwargs)
@@ -67,16 +59,13 @@ def tweets():
                  for status in statuses
                      for hashtag in status['entities']['hashtags'] ]
 
-    # Compute a collection of all words from all tweets
     words = [ w
               for t in status_texts
                   for w in t.split() ]
 
-    # print(OAUTH_TOKEN, OAUTH_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
-    # Explore the first 5 items for each...
+    dates = [ (status['created_at'])[0:19]
+                     for status in statuses ]
 
     # print(json.dumps(status_texts[0:20], indent=1))
-    # print(json.dumps(screen_names[0:5], indent=1))
-    # print(json.dumps(hashtags[0:5], indent=1))
-    # print(json.dumps(words[0:5], indent=1))
-    return render_template('twitter.html', status_texts=status_texts, screen_names=screen_names, statuses=statuses)
+
+    return render_template('twitter.html', status_texts=status_texts, screen_names=screen_names, statuses=statuses,)
